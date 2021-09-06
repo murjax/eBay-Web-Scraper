aa85adf8e29df2f7a9f7bc8351bdbf5268f44811
Ryan Murphy <murjax@gmail.com>
2021-09-05 22:37:55 -0400

    Update to support 2021 results layout

--- searcher.rb
+++ searcher.rb
@@ -11,12 +11,12 @@ CSV.open('ebay_results_list.csv', 'wb') do |csv|
     search_term = row[0].gsub(' ', '+')
     exclude_words = "-#{row[1].gsub(' ', '+-')}"
     max_price = row[2]
-    request_url = "#{search_url}#{search_term}+#{exclude_words}&LH_BIN=1&_udhi=#{max_price}"
+    request_url = "#{search_url}#{search_term}+#{exclude_words}&_udhi=#{max_price}&LH_ItemCondition=3"
     page = mechanize.get(request_url)
-    results = page.search('//li[contains(@class, "sresult")]')
+    results = page.search('//a[@class="s-item__link"]')
     results.each do |result|
-      url = result.children[1].children[1].children[1].attributes.first[1].value
-      csv << [row[0], url] unless url.include?('pulsar') || !url.include?('https')
+      url = result.attributes['href'].value
+      csv << [url]
     end
   end
 end
